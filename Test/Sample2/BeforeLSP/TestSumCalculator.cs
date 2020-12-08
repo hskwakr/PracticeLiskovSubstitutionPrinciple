4ecf09592e50d7fe08e0b4a1e66cdbd425295a44
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-12-08 18:43:23 +0900

    Add codes of BeforeLSP in Sample2
    and tests

---
+++ Test/Sample2/BeforeLSP/TestSumCalculator.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using PracticeLiskovSubstitutionPrinciple.Sample2.BeforeLSP;
+
+namespace Test.Sample2.BeforeLSP
+{
+    public class TestSumCalculator
+    {
+        public class CalculateMethod
+        {
+            [Fact]
+            public void Calculate_Calculated()
+            {
+                int[] data = new int[] {
+                    5, 7, 9, 8, 1, 6, 4
+                };
+
+                SumCalculator calc = new SumCalculator(data);
+
+                int expected = 40;
+                Assert.Equal(expected, calc.Calculate());
+            }
+        }
+    }
+}
